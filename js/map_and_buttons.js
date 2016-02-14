96a9ab5eb0d62d4fa463d160d3eb8f9f657725d5
mardarolya <omskazka@gmail.com>
2016-02-14 19:20:00 +0200

    Подключен скрипт
    1. Анимация выезда формы сделана без скрипта
    2. Скриптом загружается карта на фон
    3. Скриптом оживлены кнопки + и - на форме

---
+++ js/map_and_buttons.js
@@ -0,0 +1,41 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+        center: [34.8697395, -111.7609896], // Седона
+        zoom: 15
+        });
+}
+
+var form = document.querySelector(".search-hotel-form");
+var AdultCount = form.querySelector(".adult-count");
+var ChildCount = form.querySelector(".child-count");
+
+var MinusAdult = form.querySelector(".minus-adult");
+var PlusAdult = form.querySelector(".plus-adult");
+var MinusChild = form.querySelector(".minus-child");
+var PlusChild = form.querySelector(".plus-child");
+
+MinusAdult.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (Number(AdultCount.value) > 0) {
+    AdultCount.value = Number(AdultCount.value) - 1
+  }
+});
+
+PlusAdult.addEventListener("click", function(event) {
+  event.preventDefault();
+  AdultCount.value = Number(AdultCount.value) + 1
+});
+
+MinusChild.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (Number(ChildCount.value) > 0) {
+    ChildCount.value = Number(ChildCount.value) - 1
+  }
+});
+
+PlusChild.addEventListener("click", function(event) {
+  event.preventDefault();
+  ChildCount.value = Number(ChildCount.value) + 1
+});
